65d33964d89590acfded3ad76cf94f1f69c301cd
George Brata <bratageorge@gmail.com>
2020-11-11 18:57:37 +0200

    Updated website

--- js/main.js
+++ js/main.js
@@ -32,7 +32,21 @@ $(document).ready(function(){
 
     // -------   Signup Form-----//
     $(document).ready(function() {
+        function getRndInteger(min, max) {
+            return Math.floor(Math.random() * (max - min) ) + min;
+        }
+
+        const LAUNCH_DATE  = '2020-11-01';
+
         $('#mc_embed_signup').find('form').ajaxChimp();
+        
+        const diffInMs   = new Date() - new Date(LAUNCH_DATE)
+        const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
+
+        $('#tenis-games').text(diffInDays.toFixed(0) * 4)
+        $('#fotbal-games').text(diffInDays.toFixed(0) * 3)
+        $('#tenis-progress').width(100 - (diffInDays/4).toFixed(0)+'%');
+        $('#fotbal-progress').width(100 - (diffInDays/2).toFixed(0)+'%');
     });      
 
  });
